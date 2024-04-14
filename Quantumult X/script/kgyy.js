d84def0982218c0e960ac6b15dfe3cf824b4ab96
xy5120 <42316138+xy5120@users.noreply.github.com>
2024-04-14 21:53:55 +0800

    Update kgyy.js

--- Quantumult X/script/kgyy.js
+++ Quantumult X/script/kgyy.js
@@ -1,16 +1,17 @@
 /******************************
 
 脚本功能：酷狗音乐——解锁VIP
-软件版本：1.7
-更新时间：2022-11-25
+软件版本：12.2.0
+更新时间：2024.04.14
 
 
 *******************************
 
 [rewrite_local]
 
-http:\/\/api\.chuangqi\.store\/ url script-response-body https://raw.githubusercontent.com/xy5120/Quantumult X/script/kgyy.js
+http:\/\/api\.chuangqi\.store\/ url script-response-body https://raw.githubusercontent.com/xy5120/IOS_Rule/main/Quantumult%20X/script/kgyy.js
 
+                            
 [mitm] 
 
 hostname = api.chuangqi.store
